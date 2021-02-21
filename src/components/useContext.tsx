e24fe367be3a30b4c2804e6a2906dbc7e3892db1
Prashanth Naika <pnaika@egen.solutions>
2021-02-21 00:08:12 -0700

    initial commit

---
+++ src/components/useContext.tsx
@@ -0,0 +1,2 @@
+// https://blog.webdevsimplified.com/2020-06/use-context/
+export {}
---
+++ src/components/useState.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+
+const UseStateComponent = () => {
+    // this runs the very first time component renders
+    // const [count, setCount] = useState(() => {
+    //   console.log('run this method')
+    // });
+
+    // this runs every time the component renders
+    const [count, setCount] = useState(0);
+    const [state, setStateValue] = useState({
+        count: 0,
+        color: 'red'
+    });
+
+    // Overwrites the count here, even though there are two setCount within the func it will just increment by 1
+    const incrementValue = () => {
+        setCount(count + 1);
+        setCount(count + 1);
+    };
+
+    // Uses the prev value and hence decrements the value by two as expected
+    const decrementValue = () => {
+        setCount((prev) => prev - 1);
+        setCount((prev) => prev - 1);
+    };
+
+    const updateColor = () => {
+        setStateValue((prevState) => {
+            return {
+                ...prevState,
+                count: prevState.count + 1,
+            };
+        })
+    };
+
+    return (
+        <div>
+            <h1>useState Demo</h1>
+            <h4>For Counter Example</h4>
+            <span className="button-container">
+                <Button variant="contained" color="primary" onClick={incrementValue}>
+                    +
+                </Button>
+                <p>You clicked {count} times</p>
+                <Button variant="contained" color="secondary" onClick={decrementValue}>
+                    -
+                </Button>
+            </span>
+
+            <h4>For Name Example</h4>
+            <span className="button-container">
+                {state.count}, {state.color}
+                <Button variant="contained" color="secondary" onClick={updateColor}>
+                    change color
+                </Button>
+            </span>
+        </div>
+    );
+};
+
+export default UseStateComponent;
